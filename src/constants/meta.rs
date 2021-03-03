0d19697a0a0c8624b8f9b6b986f7697d56342a70
Bernd-L <git@bernd.pw>
2021-03-03 20:58:35 +0100

    Licence notice

---
+++ src/constants/meta.rs
@@ -0,0 +1,14 @@
+// Not every constant is used
+#![allow(dead_code)]
+
+// The name of the application
+pub const NAME: &'static str = "sfi-server";
+
+/// The main author of the application
+pub const AUTHOR: &'static str = "Bernd-L <git@bernd.pw>";
+
+/// The semantic-version string of the application
+pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+/// Describes the application (i.e. its use cases) in a short phrase
+pub const ABOUT: &'static str = "Manage shared food inventories";
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 mod api;
+mod constants;
 
 use actix_files::{Files, NamedFile};
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Resource, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Print licence notice
+    println!(
+        "{}\n\n{}\n{}",
+        constants::license::LICENSE_SHORT,
+        constants::license::license_notice_title(),
+        constants::license::license_notice_body()
+    );
+
     HttpServer::new(|| {
         App::new()
+            // Mount the API
             .service(api::mount())
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            // Serve the static files of the frontend
             .service(Files::new("/", "../sfi-web/public/").index_file("index.html"))
+            // Serve the index.html file on 404 (handle in the frontend itself)
             .default_service(web::get().to(serve_index))
     })
     .bind("127.0.0.1:8080")?
@@ -18,22 +28,9 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    println!("{:?}", std::env::current_dir());
-    HttpResponse::Ok().body("Hey there!")
-}
-
-// async fn serve_index() -> impl Responder {
-//     println!("{:?}", std::env::current_dir());
-//     HttpResponse::Ok().body("index.html goes here")
-// }
-
 async fn serve_index() -> impl Responder {
-    println!("{:?}", std::env::current_dir());
-    HttpResponse::Ok().body(std::fs::read("../sfi-web/public/index.html").unwrap())
+    // println!("{:?}", std::env::current_dir());
+    HttpResponse::Ok()
+        .header("X-sfi-server-version", constants::meta::VERSION)
+        .body(std::fs::read("../sfi-web/public/index.html").unwrap())
 }
