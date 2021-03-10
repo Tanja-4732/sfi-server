39d195eaf32c8391a491b5afac25ea20088ef9ea
Bernd-L <git@bernd.pw>
2021-03-10 11:30:52 +0100

    First middleware implementation

--- src/api/v1/middleware.rs
+++ src/api/v1/middleware.rs
@@ -10,7 +10,7 @@ use anyhow::anyhow;
 use anyhow::Result;
 use argonautica::{Hasher, Verifier};
 use futures::{
-    future::{ok, Ready},
+    future::{ok, Either, Ready},
     Future,
 };
 use google_authenticator::GA_AUTH;
@@ -75,53 +75,73 @@ where
     }
 
     fn call(&mut self, req: ServiceRequest) -> Self::Future {
-        println!("Hi from start. You requested: {}", req.path());
+        // We only need to hook into the `start` for this middleware.
 
-        let fut = self.service.call(req);
+        // Get the JWT cookie (if any)
+        let res = if let Some(jwt_cookie) = req.cookie("jwt") {
+            // Validate the JWT
+            if let Ok(uuid) = extract_uuid_from_jwt(jwt_cookie.value()) {
+                // Keep processing the request
+                println!("Continue with UUID: {:?}", &uuid);
+                Either::Left(self.service.call(req))
+            } else {
+                // Invalid JWT
 
-        Box::pin(async move {
-            let res = fut.await?;
+                // HttpResponse::Unauthorized().finish().body(json!({
+                //     "error": "Invalid credentials"
+                // }))
 
-            println!("Hi from response");
-            Ok(res)
-        })
+                println!("Invalid JWT: {:?}", &jwt_cookie);
+
+                Either::Right(ok(
+                    req.into_response(HttpResponse::Unauthorized().finish().into_body())
+                ))
+            }
+        } else {
+            // No JWT present
+
+            // HttpResponse::Unauthorized().body(json!({
+            //     "status": "Not logged in"
+            // }))
+
+            println!("Missing JWT");
+
+            Either::Right(ok(
+                req.into_response(HttpResponse::Unauthorized().finish().into_body())
+            ))
+        };
+
+        Box::pin(res)
     }
-}
 
-// #[get("/status")]
-// async fn handle_status(data: web::Data<AppState>, req: web::HttpRequest) -> impl Responder {
-//     // Get the JWT cookie (if any)
-//     if let Some(jwt_cookie) = req.cookie("jwt") {
-//         // Validate the JWT
-//         if let Ok(uuid) = extract_uuid_from_jwt(jwt_cookie.value()) {
-//             // Valid JWT, find user in projection
-//             if let Some(user) = {
-//                 data.users
-//                     .lock()
-//                     .unwrap()
-//                     .deref()
-//                     .get_projection()
-//                     .iter()
-//                     .find(|user| user.uuid == uuid)
-//                 // Drop the mutex lock here
-//             } {
-//                 // TODO keep processing the request
-//             } else {
-//                 // Report the missing entry in the projection
-//                 HttpResponse::InternalServerError().body(json!({
-//                     "error": "Couldn't find user in projection"
-//                 }))
-//             }
-//         } else {
-//             // Invalid JWT
-//             HttpResponse::Unauthorized().body(json!({
-//                 "error": "Invalid credentials"
-//             }))
-//         }
-//     } else {
-//         // No JWT present
-//         HttpResponse::Ok().body(json!({
-//             "status": "Not logged in"
-//         }))
-//     }
-// }
+    // fn call_a(&mut self, req: ServiceRequest) -> Self::Future {
+    //     println!("Hi from start. You requested: {}", req.path());
+
+    //     // Auth start
+    //     // Get the JWT cookie (if any)
+    //     let res = if let Some(jwt_cookie) = req.cookie("jwt") {
+    //         // Validate the JWT
+    //         if let Ok(uuid) = extract_uuid_from_jwt(jwt_cookie.value()) {
+    //             // Keep processing the request
+    //             let res = self.service.call(req);
+
+    //             println!("Hi from response");
+    //             ok(res)
+    //         } else {
+    //             // Invalid JWT
+
+    //             HttpResponse::Unauthorized().finish().body(json!({
+    //                 "error": "Invalid credentials"
+    //             }))
+    //         }
+    //     } else {
+    //         // No JWT present
+    //         HttpResponse::Ok().body(json!({
+    //             "status": "Not logged in"
+    //         }))
+    //     };
+
+    //     // Auth end
+    //     Box::pin(res)
+    // }
+}
